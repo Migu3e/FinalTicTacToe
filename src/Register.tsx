5c6b87984fbed99d91dc70fb464ddb12a2f63b91
Migu3e <itaymigul@gmail.com>
2024-08-01 14:17:03 +0300

    fixed the css and the design abit

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,28 +1,27 @@
-import X0pic from './assets/img.png'
+import X0pic from './assets/img.png';
+import './CSS/Register.css';
 
-
-
-function Card()
-{
+function Card() {
     return (
-        <div className="card">
-            <img className="card-image" src={X0pic} alt="TicTacImg"></img>
-            <h2 className="card-title">TicTacToe</h2>
-            <p className="card-text">Best TicTacToe</p>
+        <div className="card-container">
+            <div className="card">
+                <img className="card-image" src={X0pic} alt="TicTacImg"/>
+                <h2 className="card-title">TicTacToe</h2>
+                <p className="card-text">Best TicTacToe</p>
 
-            <div></div>
+                <div></div>
 
-            <div>
-                <button>הירשם</button>
+                <div>
+                    <button className="card-register">הירשם</button>
+                </div>
+
+                <div>
+                    <h6 className="card-registered">כבר רשום?</h6>
+                    <button>היכנס</button>
+                </div>
             </div>
-            <div>
-                <button>היכנס</button>
-            </div>
-
-
         </div>
-
-    )
+    );
 }
 
-export default Card
+export default Card;
