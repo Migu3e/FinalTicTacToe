ce03c6ff26707385382888874b437df0d7564c26
itay miguel <itaymigul@gmail.com>
2024-08-05 13:09:27 +0300

    seperated the Scorebourd

--- src/Scoreboard/Scorebourd.tsx
+++ src/Scoreboard/Scorebourd.tsx
@@ -1,53 +1,13 @@
-import useLocalStorage from '../useLocalStorage.tsx';
 import './Scoreboard.css';
-import ActionBar from '../NavActBar.tsx';
-import {useNavigationHelpers} from "../UseFunctions.ts";
+import ActionBar from '../NavActBar';
+import { useScoreboardLogic } from './ScoreboardLogic';
 
-
-interface Player {
-    name: string;
-    score: number;
-}
-
-
-const bubbleSortPlayersByScore = (players: Player[]): Player[] => {
-    const sortedPlayers:Player[] = [...players];
-    let n : number = sortedPlayers.length;
-    let swapped : boolean;
-
-    do {
-        swapped = false;
-        for (let i = 1; i < n; i++) {
-            if (sortedPlayers[i - 1].score < sortedPlayers[i].score) {
-                const temp = sortedPlayers[i - 1];
-                sortedPlayers[i - 1] = sortedPlayers[i];
-                sortedPlayers[i] = temp;
-                swapped = true;
-            }
-        }
-        n--;
-    } while (swapped);
-
-    return sortedPlayers;
-};
-
-const createPlayerRow = (player: Player): JSX.Element => {
-    return (
-        <tr key={player.name}>
-            <td>{player.name}</td>
-            <td>{player.score}</td>
-        </tr>
-    );
-};
-function Scoreboard()
-{
-    const [players] = useLocalStorage('players', []) as [Player[], (players: Player[]) => void];
-    const sortedPlayers = bubbleSortPlayersByScore(players);
-    const { handleBack } = useNavigationHelpers();
+function Scoreboard() {
+    const prop = useScoreboardLogic();
 
     return (
         <div className="scoreboard-container">
-            <ActionBar/>
+            <ActionBar />
             <h1>Scoreboard</h1>
             <table>
                 <thead>
@@ -57,11 +17,10 @@ function Scoreboard()
                 </tr>
                 </thead>
                 <tbody>
-                {sortedPlayers.map(createPlayerRow)}
-
+                {prop.sortedPlayers.map(prop.createPlayerRow)}
                 </tbody>
             </table>
-            <button onClick={handleBack}>Back</button>
+            <button onClick={prop.handleBack}>Back</button>
         </div>
     );
 }
